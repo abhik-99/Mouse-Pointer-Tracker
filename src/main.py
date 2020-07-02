1548f08bc187d07a93000badeb4af0c7b22a6d76
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2020-07-02 22:24:08 +0530

    Added Documentation.

--- src/main.py
+++ src/main.py
@@ -75,31 +75,37 @@ def main():
     input_path = args.input
 
     if input_path.lower() == 'cam':
-        input_feeder = input_feeder('cam')
+        input_feed = input_feeder('cam')
     else:
-        if not os.path.isfile(inputFilePath):
+        if not os.path.isfile(input_path):
             logger.error('Unable to find specified video file')
             exit(1)
-        input_feeder = input_feeder('video', inputFilePath)
+        input_feed = input_feeder('video', input_path)
     
-    modelPathDict = {'FaceDetectionModel':args.facedetectionmodel, 'FacialLandmarksDetectionModel':args.faciallandmarkmodel, 
-    'GazeEstimationModel':args.gazeestimationmodel, 'HeadPoseEstimationModel':args.headposemodel}
+    modelPathDict = {'face_detect':args.facedetectionmodel, 'face_landmark_regress':args.faciallandmarkmodel, 
+                    'head_pose':args.headposemodel, 'gaze_estimate':args.gazeestimationmodel}
     
     for pathname, filepath in modelPathDict:
         if not os.path.isfile(modelPathDict[filepath]):
-            logger.error('Unable to find specified '+path+' xml file')
+            logger.error('Unable to find specified '+pathname+' xml file')
             exit(1)
 
     #initializing models  
-    fdm = FaceDetectionModel(modelPathDict['FaceDetectionModel'], args.device, args.cpu_extension)
-    fldm = FacialLandmarksDetectionModel(modelPathDict['FacialLandmarksDetectionModel'], args.device, args.cpu_extension)
-    gem = GazeEstimationModel(modelPathDict['GazeEstimationModel'], args.device, args.cpu_extension)
-    hpem = HeadPoseEstimationModel(modelPathDict['HeadPoseEstimationModel'], args.device, args.cpu_extension)
+    fdm = FaceDetectionModel(modelPathDict['face_detect'], args.device, args.cpu_extension)
+    fldm = FacialLandmarksDetectionModel(modelPathDict['face_landmark_regress'], args.device, args.cpu_extension)
+    hpem = HeadPoseEstimationModel(modelPathDict['head_pose'], args.device, args.cpu_extension)
+    gem = GazeEstimationModel(modelPathDict['gaze_estimate'], args.device, args.cpu_extension)
     
     #initializing mouse controller
     mouse_controller = MouseController('medium','fast')
     
-    input_feeder.load_data()
+    input_feed.load_data()
+
+    #checking models 
+    fdm.check_model()
+    fldm.check_model()
+    hpem.check_model()
+    gem.check_model()
 
     #loading models / creating executable network
     fdm.load_model()
@@ -108,7 +114,7 @@ def main():
     gem.load_model()
     
     frame_count = 0
-    for ret, frame in input_feeder.next_batch():
+    for ret, frame in input_feed.next_batch():
         if not ret:
             break
         frame_count+=1
@@ -116,7 +122,19 @@ def main():
             cv2.imshow('video',cv2.resize(frame,(500,500)))
     
         key = cv2.waitKey(60)
-        croppedFace, face_coords = fdm.predict(frame.copy(), args.prob_threshold)
+        """
+        Sequence of model execution:-
+        1. Predict from each model.
+        2. Preprocess of outputs from each model.
+        3. Send the processed output to the next model.
+
+        Model Sequence:- 
+                                - Head Pose Estimation Model      -
+        Face Detection Model <                                      > Gaze Estimation Model 
+                                - Facial Landmark Detection Model -  
+        """
+
+        croppedFace, _ = fdm.predict(frame.copy(), args.prob_threshold)
 
         if type(croppedFace)==int:
             logger.error('Unable to detect the face.')
@@ -130,22 +148,22 @@ def main():
         
         new_mouse_coord, gaze_vector = gem.predict(left_eye, right_eye, hp_out)
         
-        if (not len(previewFlags)==0):
+        if (not len(preview_flags)==0):
             preview_frame = frame.copy()
-            if 'fd' in previewFlags:
+            if 'fd' in preview_flags:
                 #cv2.rectangle(preview_frame, (face_coords[0], face_coords[1]), (face_coords[2], face_coords[3]), (255,0,0), 3)
                 preview_frame = croppedFace
-            if 'fld' in previewFlags:
+            if 'fld' in preview_flags:
                 cv2.rectangle(croppedFace, (eye_coords[0][0]-10, eye_coords[0][1]-10), (eye_coords[0][2]+10, eye_coords[0][3]+10), (0,255,0), 3)
                 cv2.rectangle(croppedFace, (eye_coords[1][0]-10, eye_coords[1][1]-10), (eye_coords[1][2]+10, eye_coords[1][3]+10), (0,255,0), 3)
                 #preview_frame[face_coords[1]:face_coords[3], face_coords[0]:face_coords[2]] = croppedFace
                 
-            if 'hp' in previewFlags:
+            if 'hp' in preview_flags:
                 cv2.putText(preview_frame, 'Pose Angles: yaw:{:.2f} | pitch:{:.2f} | roll:{:.2f}'.format(hp_out[0],hp_out[1],hp_out[2]), (10, 20), cv2.FONT_HERSHEY_COMPLEX, 0.25, (0, 255, 0), 1)
             
-            if 'ge' in previewFlags:
+            if 'ge' in preview_flags:
                 x, y, w = int(gaze_vector[0]*12), int(gaze_vector[1]*12), 160
-                le =cv2.line(left_eye.copy(), (x-w, y-w), (x+w, y+w), (255,0,255), 2)
+                le = cv2.line(left_eye.copy(), (x-w, y-w), (x+w, y+w), (255,0,255), 2)
                 cv2.line(le, (x-w, y+w), (x+w, y-w), (255,0,255), 2)
                 re = cv2.line(right_eye.copy(), (x-w, y-w), (x+w, y+w), (255,0,255), 2)
                 cv2.line(re, (x-w, y+w), (x+w, y-w), (255,0,255), 2)
